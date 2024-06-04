f73a865f43225c387eb2c47ef1bdfbd015671cc7
Sajith <sajithanuradha860@gmail.com>
2024-06-04 21:26:52 +0530

    finished the nav bar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
 
 function App() {
   return (
-    <div>App</div>
+    <div className='app'  >
+      <Navbar />
+    </div>
   )
 }
 
