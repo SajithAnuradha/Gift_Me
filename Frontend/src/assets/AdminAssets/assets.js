9e5218bce15c7c0aada2fca35c6574389edfdf73
Sajith Anuradha <sajithanuradha860@gmail.com>
2024-08-15 09:25:41 +0530

    Add files via upload

---
+++ Frontend/src/assets/AdminAssets/assets.js
@@ -0,0 +1,24 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+import checkout from './checkout.png'
+import gift from './gift.png'
+import customer from './customer.png'
+import parcel from './parcel.jpeg'
+export const assets = {
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon,
+    checkout,
+    gift,
+    customer,
+    parcel
+}
+
+export const url = 'http://localhost:4000'
