7dd6204b02dc9bd756c3877b46d5ba4ba9f31abf
Sajith Anuradha <sajithanuradha860@gmail.com>
2024-08-25 15:42:07 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
 
-// const connectDB = async () => {
-//     await mongoose.connect('mongodb+srv://sajithanuradha860:443556@cluster0.gwmnair.mongodb.net/foode-ord').then(() => console.log('DB connected'))
-// }
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODBURL );
+    console.log('DB connected');
+  } catch (error) {
+    console.error('Database connection failed:', error.message);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
 
 export default connectDB;
